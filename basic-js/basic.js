6ae9993aea2a9272df3e416cb297369990a5d1dd
Andrew Komorkovich <komorkovics@gmail.com>
2023-02-14 23:09:07 +0200

    if - else if - else, nested contionals, truthy-falsy, logical operators AND, OR, NOT

--- basic-js/basic.js
+++ basic-js/basic.js
@@ -60,8 +60,95 @@ parseInt(userInput) // will convert string to number
 let rating = 3;
 
 if (rating === 3) { // <=== true of false condition
-    console.log("You win!");
+    console.log("You won gold medal!");
+} else if (rating === 2) {
+    console.log('You won silver medal!')
+} else if (rating === 1) {
+    console.log('You won bronze!')
+} else {
+    console.log('Sorry, you lose!')
 }
+
+//**Nested conditions **//
+let password = prompt('Enter a password');
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log('Valid password!')
+    }
+    else {
+        console.log('Password cannot contain spaces!')
+    }
+} else {
+    console.log('Password must be 6+ characters long!')
+}
+
+//** Truthy % Falsy**/
+
+// - All JS values have an inherent trytheness or falsyness about then!
+// Falsy values: (false) (0) ('' / empry string) (null) (undefined) (NaN)
+// Trythy - everything else is truthy!
+
+if (userInput) { // <== if userInput value is booleand true - run this code, if it is boolean false - run another code
+    console.log('Truthy!')
+} else {
+    console.log('Falsy!')
+}
+
+//** Logical operator AND (&&)**//
+
+// - Both side need to be true, for the entire thing to be to true!
+
+// (1 <= 4 && 'a' === 'a') <== true
+// (9 > 10 && 9 >= 9) <== false
+// ('abc'.length === 3 && 1 + 1 === 4) < == false
+
+let usersInput = prompt('Enter your password here');
+
+if (usersInput.length >= 6 && usersInput.indexOf(' ') === -1) {
+    console.log('Valid password!')
+} else {
+    console.log('Password is invalid!')
+}
+
+let mysteryString = 'Please7';
+
+if (mysteryString[0] === 'P' && mysteryString.length >= 5 && mysteryString.indexOf('7') !== -1) {
+    console.log('Mystery String is revealed!');
+}
+
+//** Logical operator OR (||)**/
+
+// - If one side is true, the entire thing is true
+// (1 !== 1 || 10 === 10) <== true
+// (10/2 === 5 || null) <== true
+// (0 || undefined) <== false
+
+let age = 10;
+
+if (age >= 0 && age <= 5 || age >= 65) {
+    console.log('You get in for free!')
+} else if (age >= 6 && age <= 10 || age === 'veteran') {
+    console.log('You het in for a fee $10')
+} else {
+    console.log('You get in for a fee $20')
+}
+
+//** Logical operator NOT (!) **/
+
+// - !expression return true if expression is false
+// (!null) - true (НЕ null)
+// (!(0 === 0)) - false
+// (!(3 <= 4)) - false
+
+if (!(age >= 0 && age <= 5 || age >= 65)) {
+    console.log('You are not a baby of a senior!')
+} // <=== the (age >= 0 && age <= 5 || age >= 65) part runs first and an output will the TRUE
+  // <=== then ! will turn it around and get it FALSE, so (NOT THIS (age >= 0 && age <= 5 || age >= 65))
+
+//** Switch **//
+
+
 
 
 
