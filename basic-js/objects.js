3cbc90d6782379bac13d927a675ffdd29dcbf32c
Andrew Komorkovich <komorkovics@gmail.com>
2023-02-19 22:00:26 +0200

    accesing, modofying and adding data in the objects, arrays + objects

--- basic-js/objects.js
+++ basic-js/objects.js
@@ -4,6 +4,10 @@
 // - Properties are a KEY-VALUE pair
 // - Rather that acceccsing data using an index, we use KEYS.
 
+// Creating - 1st option: let user = new Object();
+// Creating - 2nd option: let use = {}; // an empty shell of an object
+
+
 // let comment = {
 //     userName: 'Mick Jagger',
 //     downVote: 19,
@@ -38,6 +42,37 @@ const restaurant = {
     state: 'NY',
     zipcode: '11206',
 };
+
+//** Modofying Objects**//
+
+const midterms = {
+    antonio: 79,
+    daniel: 68,
+    alfy: 96
+};
+
+//* Updating *//
+
+midterms.antonio = 'B+' // output antonio: 'B+'
+midterms['daniel'] = 'C' // output daniel: 'C'
+
+//* Adding new *//
+
+midterms.andrew = 'A+' // output andrew: 'A+' (new key andrew with value 'A+' is added)
+midterms.yury = 'A+' // output yury: 'A+'(new key yury with value 'A+' is added to object midterms)
+
+//** Arrays + Objects and accesing**//
+
+const comments = [
+    { userName: 'Tammy', comment: 'lol guys', votes: 9 },
+    { userName: 'FishBoi', comment: 'glub glub', votes: 126651 }
+];
+
+console.log(comments[1].comment);
+console.log(comments[0]['votes']);
+
+
+
 
 
 
