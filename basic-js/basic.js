a58d1f13e00b6e578313b4f24c76f8673d48b1b3
Andrew Komorkovich <komorkovics@gmail.com>
2023-02-23 22:52:55 +0200

    nested loops, while loop, break - keyword

--- basic-js/basic.js
+++ basic-js/basic.js
@@ -1,184 +1,184 @@
-// String index //
+// // String index //
 
-let animal = "zebra";
-animal[3]; // ==> 'r'
-animal[0]; // ==> 'z'
+// let animal = "zebra";
+// animal[3]; // ==> 'r'
+// animal[0]; // ==> 'z'
 
-animal.length // ==> 5
-animal.length - 1 // ==> highest index of a string
+// animal.length // ==> 5
+// animal.length - 1 // ==> highest index of a string
 
-//String medhods//
-let userName = 'Andrew';
-userName.toUpperCase();
-userName.toLowerCase();
-userName.trim(); // ==> removes white spaces from the beggining or end of the string.
-userName.indexOf('A'); // ==> 0
-userName.indexOf('drew'); // ==> 2
-userName.indexOf('m'); // ==> -1 (character not found)
-userName.slice(3); // ==> 'rew' starts from index 3 and goes till the end of the string ('rew' is a new returned string)
-userName.slice(1, 4); // ==> 'ndr' - will start from index 1 and will go BEFORE and indec 4 (from index 1 - 3, not including 4)
-userName.slice(-3); // ==> 'rew' - working backwards from the end
-userName.replace('A', 'B'); // ==> Bandrew; will ONLY replace first appearance
-userName.repeat(2); // ==> 'AndrewAndrew'
-userName.trim().toUpperCase();
+// //String medhods//
+// let userName = 'Andrew';
+// userName.toUpperCase();
+// userName.toLowerCase();
+// userName.trim(); // ==> removes white spaces from the beggining or end of the string.
+// userName.indexOf('A'); // ==> 0
+// userName.indexOf('drew'); // ==> 2
+// userName.indexOf('m'); // ==> -1 (character not found)
+// userName.slice(3); // ==> 'rew' starts from index 3 and goes till the end of the string ('rew' is a new returned string)
+// userName.slice(1, 4); // ==> 'ndr' - will start from index 1 and will go BEFORE and indec 4 (from index 1 - 3, not including 4)
+// userName.slice(-3); // ==> 'rew' - working backwards from the end
+// userName.replace('A', 'B'); // ==> Bandrew; will ONLY replace first appearance
+// userName.repeat(2); // ==> 'AndrewAndrew'
+// userName.trim().toUpperCase();
 
-//Template Literals//
-let product = 'apple';
-let quantity = 5;
-let price = 2.00;
+// //Template Literals//
+// let product = 'apple';
+// let quantity = 5;
+// let price = 2.00;
 
-let total = `You bought ${quantity} ${product}. Total is ${quantity * price}`; // ==>
-// ==> 'You bought 5 apple. Total is 10.00';
+// let total = `You bought ${quantity} ${product}. Total is ${quantity * price}`; // ==>
+// // ==> 'You bought 5 apple. Total is 10.00';
 
-//**null**//
-// - Intentional (преднамеренный) absence of ny value (we can set something to null, like let userName = null)
-// - Must be assigned
+// //**null**//
+// // - Intentional (преднамеренный) absence of ny value (we can set something to null, like let userName = null)
+// // - Must be assigned
 
-//**undefined**/
-// - Variables that do not have an assigned value are undefined, like let x; (x is variable, but it is undefined)
+// //**undefined**/
+// // - Variables that do not have an assigned value are undefined, like let x; (x is variable, but it is undefined)
 
-// ** Comparisons ** //
-// (>), (<), (>=), (<=), (==), (!=), (===), (!==)
+// // ** Comparisons ** //
+// // (>), (<), (>=), (<=), (==), (!=), (===), (!==)
 
-// == (equality)
-// - check for equality of value, but NOT equality of type
-// - It coerces both values to the same type and then compares them.
-// - This can lead to some unexpected results (like when comapring two strings letters)
+// // == (equality)
+// // - check for equality of value, but NOT equality of type
+// // - It coerces both values to the same type and then compares them.
+// // - This can lead to some unexpected results (like when comapring two strings letters)
 
-// === (strict equaluty)
-// - check for equality of value and type
-// - try to ALWAYS use === equality!
+// // === (strict equaluty)
+// // - check for equality of value and type
+// // - try to ALWAYS use === equality!
 
-// =======================================
+// // =======================================
 
-let userInput = prompt("please enter your name"); // Enter 35 // promprt returns string!!
+// let userInput = prompt("please enter your name"); // Enter 35 // promprt returns string!!
 
-parseInt(userInput) // will convert string to number
+// parseInt(userInput) // will convert string to number
 
-//=======================================
+// //=======================================
 
-let rating = 3;
+// let rating = 3;
 
-if (rating === 3) { // <=== true of false condition
-    console.log("You won gold medal!");
-} else if (rating === 2) {
-    console.log('You won silver medal!')
-} else if (rating === 1) {
-    console.log('You won bronze!')
-} else {
-    console.log('Sorry, you lose!')
-}
+// if (rating === 3) { // <=== true of false condition
+//     console.log("You won gold medal!");
+// } else if (rating === 2) {
+//     console.log('You won silver medal!')
+// } else if (rating === 1) {
+//     console.log('You won bronze!')
+// } else {
+//     console.log('Sorry, you lose!')
+// }
 
-//** if statement shortened version!**//
+// //** if statement shortened version!**//
 
-let accessAllowed = (age >= 18) ? true : false;
-// - if (age >= 18) is true? ==> run true;
-// - else (:) ==> run false;
+// let accessAllowed = (age >= 18) ? true : false;
+// // - if (age >= 18) is true? ==> run true;
+// // - else (:) ==> run false;
 
-//**Nested conditions **//
-let password = prompt('Enter a password');
+// //**Nested conditions **//
+// let password = prompt('Enter a password');
 
-if (password.length >= 6) {
-    if (password.indexOf(' ') === -1) {
-        console.log('Valid password!')
-    }
-    else {
-        console.log('Password cannot contain spaces!')
-    }
-} else {
-    console.log('Password must be 6+ characters long!')
-}
+// if (password.length >= 6) {
+//     if (password.indexOf(' ') === -1) {
+//         console.log('Valid password!')
+//     }
+//     else {
+//         console.log('Password cannot contain spaces!')
+//     }
+// } else {
+//     console.log('Password must be 6+ characters long!')
+// }
 
-//** Truthy % Falsy**/
+// //** Truthy % Falsy**/
 
-// - All JS values have an inherent trytheness or falsyness about then!
-// Falsy values: (false) (0) ('' / empry string) (null) (undefined) (NaN)
-// Trythy - everything else is truthy!
+// // - All JS values have an inherent trytheness or falsyness about then!
+// // Falsy values: (false) (0) ('' / empry string) (null) (undefined) (NaN)
+// // Trythy - everything else is truthy!
 
-if (userInput) { // <== if userInput value is booleand true - run this code, if it is boolean false - run another code
-    console.log('Truthy!')
-} else {
-    console.log('Falsy!')
-}
+// if (userInput) { // <== if userInput value is booleand true - run this code, if it is boolean false - run another code
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
 
-//** Logical operator AND (&&)**//
+// //** Logical operator AND (&&)**//
 
-// - Both side need to be true, for the entire thing to be to true!
+// // - Both side need to be true, for the entire thing to be to true!
 
-// (1 <= 4 && 'a' === 'a') <== true
-// (9 > 10 && 9 >= 9) <== false
-// ('abc'.length === 3 && 1 + 1 === 4) < == false
+// // (1 <= 4 && 'a' === 'a') <== true
+// // (9 > 10 && 9 >= 9) <== false
+// // ('abc'.length === 3 && 1 + 1 === 4) < == false
 
-let usersInput = prompt('Enter your password here');
+// let usersInput = prompt('Enter your password here');
 
-if (usersInput.length >= 6 && usersInput.indexOf(' ') === -1) {
-    console.log('Valid password!')
-} else {
-    console.log('Password is invalid!')
-}
+// if (usersInput.length >= 6 && usersInput.indexOf(' ') === -1) {
+//     console.log('Valid password!')
+// } else {
+//     console.log('Password is invalid!')
+// }
 
-let mysteryString = 'Please7';
+// let mysteryString = 'Please7';
 
-if (mysteryString[0] === 'P' && mysteryString.length >= 5 && mysteryString.indexOf('7') !== -1) {
-    console.log('Mystery String is revealed!');
-}
+// if (mysteryString[0] === 'P' && mysteryString.length >= 5 && mysteryString.indexOf('7') !== -1) {
+//     console.log('Mystery String is revealed!');
+// }
 
-//** Logical operator OR (||)**/
+// //** Logical operator OR (||)**/
 
-// - If one side is true, the entire thing is true
-// (1 !== 1 || 10 === 10) <== true
-// (10/2 === 5 || null) <== true
-// (0 || undefined) <== false
+// // - If one side is true, the entire thing is true
+// // (1 !== 1 || 10 === 10) <== true
+// // (10/2 === 5 || null) <== true
+// // (0 || undefined) <== false
 
-let age = 10;
+// let age = 10;
 
-if (age >= 0 && age <= 5 || age >= 65) {
-    console.log('You get in for free!')
-} else if (age >= 6 && age <= 10 || age === 'veteran') {
-    console.log('You het in for a fee $10')
-} else {
-    console.log('You get in for a fee $20')
-}
+// if (age >= 0 && age <= 5 || age >= 65) {
+//     console.log('You get in for free!')
+// } else if (age >= 6 && age <= 10 || age === 'veteran') {
+//     console.log('You het in for a fee $10')
+// } else {
+//     console.log('You get in for a fee $20')
+// }
 
-//** Logical operator NOT (!) **/
+// //** Logical operator NOT (!) **/
 
-// - !expression return true if expression is false
-// (!null) - true (НЕ null)
-// (!(0 === 0)) - false
-// (!(3 <= 4)) - false
+// // - !expression return true if expression is false
+// // (!null) - true (НЕ null)
+// // (!(0 === 0)) - false
+// // (!(3 <= 4)) - false
 
-if (!(age >= 0 && age <= 5 || age >= 65)) {
-    console.log('You are not a baby of a senior!')
-} // <=== the (age >= 0 && age <= 5 || age >= 65) part runs first and an output will the TRUE
-// <=== then ! will turn it around and get it FALSE, so (NOT THIS (age >= 0 && age <= 5 || age >= 65))
+// if (!(age >= 0 && age <= 5 || age >= 65)) {
+//     console.log('You are not a baby of a senior!')
+// } // <=== the (age >= 0 && age <= 5 || age >= 65) part runs first and an output will the TRUE
+// // <=== then ! will turn it around and get it FALSE, so (NOT THIS (age >= 0 && age <= 5 || age >= 65))
 
-//** Switch statement**//
+// //** Switch statement**//
 
-const day = 5;
+// const day = 5;
 
-switch (day) {
-    case 1:
-        console.log('Monday');
-        break;
-    case 2:
-        console.log('Tuesday');
-        break;
-    case 3:
-        console.log('Wednesday');
-        break;
-    case 4:
-        console.log('Thursday');
-        break;
-    case 5:
-        console.log('Friday');
-        break;
-    case 6:
-    case 7:
-        console.log('Weekend!');
-        break;
-    default:
-        console.log('The wrong number!');
-}
+// switch (day) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//     case 7:
+//         console.log('Weekend!');
+//         break;
+//     default:
+//         console.log('The wrong number!');
+// }
 
 
 
--- basic-js/loops.js
+++ basic-js/loops.js
@@ -27,4 +27,62 @@ for (let i = animals.length - 1; i >= 0; i--) {
     console.log(animals[i]);
 } // <=== will go backwards and print everithing from the end to the start
 
+//** Nested loops (for loop)**//
+
+for (let i = 1; i < 5; i++) {
+    console.log(`This OUTER loop runs ${i} times`);
+    for (let j = 1; j < 3; j++) {   // <=== j runs all the loops (3 times) for every one single i iteration
+        console.log(`This INNER loop runs ${j} times`);
+    }
+}
+
+const seatingChart = [
+    ['Kristen', 'Eric', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erica']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`This is ROW# ${i + 1}`);
+    console.log('=====================')
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j]);
+    }
+}
+
+//** The WHILE loop  **//
+
+let num = 0;
+while (num < 0) {   // <=== The while loop continue to running while (conditions) is true
+    console.log(num);
+    num++;
+}
+
+let secretWord = 'BabbyHippo';
+
+let guess = prompt('Enter the secret word!...');
+while (guess !== secretWord) {                 // <=== while this is true..
+    guess = prompt('Not correct! Try again!')  //.. <=== this part will always run
+}
+
+console.log('Congratulations! You guessed a secret word!');
+
+//** BREAK key word **//
+
+let input = prompt('Hey there, just say a word');
+while (true) {
+    input = prompt(input);
+    if (input.toLowerCase() === 'stop copying me!') break;
+}
+
+console.log('Ok! You Won!');
+
+for (let i = 1; i <= 1000; i++) {
+    console.log(i);
+    if (i >= 100) break;
+}
+
+
+
 
