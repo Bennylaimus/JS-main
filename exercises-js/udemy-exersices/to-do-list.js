6eacdfef59588815cfba581aed9f47d516d53716
Andrew Komorkovich <komorkovics@gmail.com>
2023-02-26 22:43:39 +0200

    started to-do-list exercise

---
+++ exercises-js/udemy-exersices/to-do-list.js
@@ -0,0 +1,16 @@
+// ** TASKS ** //
+
+//1 step - prompt with "What would you likem to do?" showed to a user
+//2 step - user can decide, what do to:
+// 2.1 - 'new' - add a new to do thing (a prompt 'Enter new todo appears') == > user writes new to do in the prompt, in console - 'Collect eggs appers' and 'What would you like to do?' appears again.
+// 2.2 - 'list' - console is showing a following message:
+// ***********
+// 0. Feed Turtles
+// 1. Feed Chickens
+// 2. Feed cats
+// Number is important as we want to delete to do by using this index number!
+// 2.3 'delete' - promp user a message - 'Enter a index you want to delete!', after entering index (1) message 'To do Removed' appears in a console and message 'What would you like to do appears again'
+// 2.4 'quit' - quits a list and prints in the console 'Ok, you quit!'
+
+
+
