e652746a324584cdc7d493abc7de1c2839698e83
Andrew Komorkovich <komorkovics@gmail.com>
2023-02-28 22:58:26 +0200

    basics of functions. defining and running function

---
+++ functions-js/basic-functions.js
@@ -0,0 +1,16 @@
+//** Functions basics **//
+
+// 1. Defining functions:
+
+// function funcName() {
+//  do something
+//};
+
+function ungryGrumpus() {
+    console.log('I am an angry Grupmus');
+    console.log('I will eat you!');
+    console.log('Muahhahha!!!');
+}
+
+// 2. Run function:
+//ungryGrumpus();
