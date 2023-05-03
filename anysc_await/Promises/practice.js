66facdfdfccff6a2f9f5438ae365a03874a8694c
Andrew Komorkovich <komorkovics@gmail.com>
2023-05-03 22:40:26 +0300

    repeat and understand async await try - catch

--- anysc_await/Promises/practice.js
+++ anysc_await/Promises/practice.js
@@ -1,45 +1,52 @@
-// const fakeRequestPromise = (url) => {
-//     return new Promise((resolve, reject) => {
-//         const delay = Math.floor(Math.random() * (4500)) + 500;
-//         setTimeout(() => {
-//             if (delay > 4000) {
-//                 reject('Connection Timeout :(')
-//             } else {
-//                 resolve(`Here is your fake data from ${url}`)
-//             }
-//         }, delay)
-//     })
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 1000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve('resolved');
+            }
+        }, delay)
+    })
+};
+
+// We can CAPTURE a value returned in resolve in variable and the worj with that variable
+
+// async function receivingData() {
+//     let data = await fakeRequestPromise('myhouse/page1');
+//     console.log(data);
 // };
 
-// CREATING OUR OWN PROMISES //
+// receivingData(); // prints resolved
 
-// FIRST 'PART' OF PROMISES
-//1 step - kew word new Promise
-//2 step - new Promise expects us to pass in 2 callback functions as a parametrs
-// First function (usually called 'resolved') will handle only 'good' outcomes ('resolved', 'fulfilled')
-// Second function (usually called 'rejected') will handle only 'bad' outcomes ('rejected')
-//3 step - we write logic - when, at what conditions resolved will work, and when reject will work
-// We can even call this functions by their own simply write resolve() inside a Promise
-//4 step - in the 'resolve' and 'reject' functions we can pass their own parameters like this: resolve('Good output');
-// or rejected('Bad output')
+// How do handle errors in async functions? (.catch / reject)
 
-// SECOND 'PART' OF PROMISES:
-//1 step - we can create our own function, which will accept for example one parametr (url) and this function will return our Promise
-// const fakeReguestData = (ulr) => {
-//     return new Promise ...
-//}
-//2 step - all 'positive', resolved values will be captured in .then(() => {})
-//2.1 step - (see FIRST 'PART' step 4): .then(data) => {console.log(data)} - data will store 'Goog output' value!
-//3 step - all 'negative', rejected values will be captured in .catch(() => {});
-//2.2 step - (see FIRST 'PART' step 4): .catch(error) => {console.log(error)} - data will store 'Goog output' value!
-//4 step .then and .catch methods are called on the functions:
-// fakeReguestData('www.google.com').then((data) => {console.log(data)})
-// fakeReguestData('www.google.com').catch((error) => {console.log(error)})
+// We can actually do it with a try - catch:
 
-//Smoothier syntax using return:
+// async function receivingData() {
+//     await fakeRequestPromise('myhouse/page1');
+//     try {
+//         erororr.log('hello') //  some error appears
+//     }
+//     catch (error) {
+//         console.log('An Error appeared', error) // error is the value of the error in try itself (erororr.log('hello') in or case)
+//     }
+// };
 
-//fakeReguestData('www.google.com/page1')
-//  .then(() => return fakeReguestData('www.google.com/page2'))
-//  .then(() => return fakeReguestData('www.google.com/page3'))
-//  .then(() => return fakeReguestData('www.google.com/page4'))
-//  .catch((error) => console.log(error))          // only one .catch can be used in this case in the end
+// receivingData();
+
+// How it will work with our fakeRequestPromise()
+
+async function receiveData() {
+    try {
+        await fakeRequestPromise('myhomepage/page1');
+        await fakeRequestPromise('myhomepage/page2'); // if at some point error (reject) will appear it will be caught 
+        await fakeRequestPromise('myhomepage/page3'); // in catch and we will see it and will be able to check what exactly this erro is!
+    }
+    catch (error) {
+        console.log('Error is caught');
+        console.log(error);
+    }
+};
+
