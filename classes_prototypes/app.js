3f38194a1d68467ade2c73484f2c95714692c81b
Andrew Komorkovich <komorkovics@gmail.com>
2023-05-16 22:56:44 +0300

    intro to protorypes and __proto__, started OOP & Factory function

---
+++ classes_prototypes/app.js
@@ -0,0 +1,38 @@
+// ============================ 1st Lesson: Intro to prototypes ================================ // 
+
+//Defining out own behavior of an array as an example:
+// const arr = [1, 2, 3];
+// arr.sing = function () {
+//     console.log('LA LA LA');
+// }
+
+// // arr (which is an array will now have a 'sing' function in array methods)
+
+// const doc = document.body
+// console.dir(doc);
+
+// String.prototype.grumpus = () => {
+//     alert('Go Away you nasty Grumpus!');
+// }
+
+// const cat = 'Blue';
+// cat.grumpus() // alert (Go Away ...)
+
+// String.prototype.yell = function () {
+//     console.log(this);
+// }
+
+// String.prototype.yell = function () {
+//     return `OMG! ${this.toUpperCase()} !!! OMGIIIII!`
+// }
+
+// Array.prototype.pop = function () {
+//     console.log('I will never ever pop it up!');
+// }
+
+// const arr3 = [7, 8, 9];
+
+// console.log(arr3.pop()); // output ==> 'I will never ever pop it up!'
+
+// ============================ 2nd Lesson: Intro Into OOP & Factory function ================================ // 
+
