56536f86f7d283f5f4f1271dffae50b396b0f4b5
Andrew Komorkovich <komorkovics@gmail.com>
2024-01-10 21:35:42 +0200

    Repeating. Section 27. Lessons 281 - 283

--- repeat-js-2024/main.js
+++ repeat-js-2024/main.js
@@ -51,11 +51,102 @@
 
 // =================================================================
 
+// const fakeRequestPromise = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * (2000)) + 500;
+//         setTimeout(() => {
+//             if (delay > 2000) {
+//                 reject('Connection timeout');
+//             } else {
+//                 resolve(`Here is your data from ${url}`)
+//             }
+//         }, delay)
+//     })
+// }
 
+// fakeRequestPromise('books.com/pag1')
+//     .then(() => {
+//         console.log('Promise resolved on page 1, .then() applies')
+//         fakeRequestPromise('books.com/page2')
+//             .then(() => {
+//                 console.log('Promise resolved on page 2, .then() applies')
+//             })
+//         fakeRequestPromise('books.com/page3')
+//             .then(() => {
+//                 console.log('Promise resolved on page 3, .then() applies')
+//             })
+//             .catch(() => {
+//                 console.log('Promise rejected on page 3, .catch() applies');
+//             })
+//             .catch(() => {
+//                 console.log('Promise rejected on page 2, .catch() applies');
+//             })
+//     })
+//     .catch(() => {
+//         console.log('Promise rejected on page 1, .catch() applies');
+//     });
 
+// fakeRequestPromise('books.com/page1')
+//     .then((data) => {
+//         console.log('Resolve of Promise - page 1')
+//         console.log(data)
+//         return fakeRequestPromise('books.com/page2')
+//     })
+//     .then((data) => {
+//         console.log('Resolve of Promise - page 2')
+//         console.log(data)
+//         return fakeRequestPromise('books.com/page3')
+//     })
+//     .then((data) => {
+//         console.log('Resolve of Promise - page 3')
+//         console.log(data)
+//     })
+//     .catch((error) => {
+//         console.log('Connection timedout, Promise rejected', error)
+//     })
 
+// const myRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, 1000)
+//     })
+// }
 
+// const result = myRequest();
 
+// myRequest('dogs.com/page1')
+
+const delayedColor = (color) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(document.body.style.backgroundColor = color)
+        }, 1500)
+    })
+};
+
+delayedColor('red')
+    .then(() => {
+        return delayedColor('blue')
+    })
+    .then(() => {
+        return delayedColor('orange')
+    })
+    .then(() => {
+        return delayedColor('blue')
+    })
+    .then(() => {
+        return delayedColor('aquamarine')
+    })
+    .then(() => {
+        return delayedColor('orange')
+    })
+    .then(() => {
+        return delayedColor('olive')
+    })
+    .then(() => {
+        return delayedColor('yellow')
+    })
 
 
 
