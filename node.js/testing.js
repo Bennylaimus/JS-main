52ec19482ded00c93a6d2f54abb4fa33594e705e
Andrew Komorkovich <komorkovics@gmail.com>
2024-03-24 14:18:38 +0200

    repeat of node.js basics --process.argv[2], slice[2], mkdirSync, makeFileSync

---
+++ node.js/testing.js
@@ -0,0 +1,8 @@
+const test = require("fs");
+
+const newFolder = process.argv[2];
+
+test.mkdirSync(`${newFolder}` || 'Project');
+test.writeFileSync(`${newFolder}/test.html`, '');
+test.writeFileSync(`${newFolder}/test.css`, '');
+test.writeFileSync(`${newFolder}/test.js`, '');
