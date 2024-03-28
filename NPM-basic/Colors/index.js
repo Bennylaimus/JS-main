2d86e449da6d00d1f366bc67ec6b37faf1f6c579
Andrew Komorkovich <komorkovics@gmail.com>
2024-03-28 22:56:11 +0200

    npm-basics

---
+++ NPM-basic/Colors/index.js
@@ -0,0 +1,4 @@
+const colors = require('colors');
+
+console.log('hello'.red);
+console.log('Run the bass here'.rainbow);
---
+++ NPM-basic/Jokes/index.js
@@ -0,0 +1,7 @@
+const jokes = require('give-me-a-joke');
+const myColor = require('../Colors/colors');
+// jokes.getRandomCNJoke(function (joke) {
+//     console.log(joke);
+// });
+
+console.log('hello'.yellow);
