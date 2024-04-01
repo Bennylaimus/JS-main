211b5a60ff0af885a7c95ca86ef21428da10a450
Andrew Komorkovich <komorkovics@gmail.com>
2024-04-01 17:34:25 +0300

    more practice with express path params req.params

--- express_servers/firstapp/index.js
+++ express_servers/firstapp/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-    console.log('We just got a new request');
+// app.use((req, res) => {
+//     res.send({ color: 'red', isAdmin: true });
+// })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to a home page!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('Welcome to dogs section, woof!');
+})
+
+app.get('/cats', (req, res) => {
+    res.send('Welcome to cats section, meow!');
+});
+
+app.get('/r/:subpath/:postID', (req, res) => { // subpath = 'chickens'
+    const subtheme = req.params.subpath
+    const postId = req.params.postID
+    console.log(req.params);
+    res.send(`<h1>Post with ${postId} to a ${subtheme} theme`);
 })
 
 app.listen(3000, () => {
     console.log('Listening on port: 3000');
 })
+
+
