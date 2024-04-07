7140c4ac1ea3361ab55988356b3bc20934954cb9
Andrew Komorkovich <komorkovics@gmail.com>
2024-04-07 11:54:55 +0300

    ejs syntax and logic basics

--- express_servers/express_ejs/index.js
+++ express_servers/express_ejs/index.js
@@ -3,10 +3,25 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')); // __dirname + /views (we are putting togethew index.js file's directory with /views), so we can tun it from othe working directories
+app.set('views', path.join(__dirname, '/views')); // __dirname + /views (we are putting togethew index.js file's directory with /views), so we can run it from other working directories
 
 app.get('/', (req, res) => {
     res.render('home.ejs');        // by defauls it will 'look' in views/home.ejs (in views directory)
+})
+
+app.get('/random', (req, res) => {
+    const number = Math.floor(Math.random() * 10) + 1
+    res.render('random.ejs', { rand: number });
+})
+
+app.get('/random2', (req, res) => {
+    const number = Math.floor(Math.random() * 10) + 1
+    res.render('random2.ejs', { num: number });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const subtheme = req.params.subreddit
+    res.render('subtheme', { subtheme });
 })
 
 app.listen(3000, (req, res) => {
