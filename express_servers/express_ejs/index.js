43369d2ab54e64689af404eee5eb53c9302357e5
Andrew Komorkovich <komorkovics@gmail.com>
2024-04-09 23:36:34 +0300

    more ejs templating practice

--- express_servers/express_ejs/index.js
+++ express_servers/express_ejs/index.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const allData = require('./data.json');
+console.log(allData);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')); // __dirname + /views (we are putting togethew index.js file's directory with /views), so we can run it from other working directories
 
 app.get('/', (req, res) => {
     res.render('home.ejs');        // by defauls it will 'look' in views/home.ejs (in views directory)
-})
+});
 
 app.get('/random', (req, res) => {
     const number = Math.floor(Math.random() * 10) + 1
-    res.render('random.ejs', { rand: number });
-})
+    res.render('random.ejs', { number }); // or {num: number}
+});
 
 app.get('/random2', (req, res) => {
     const number = Math.floor(Math.random() * 10) + 1
-    res.render('random2.ejs', { num: number });
-})
+    res.render('random2.ejs', { num: number }); // of {number}
+});
 
 app.get('/r/:subreddit', (req, res) => {
     const subtheme = req.params.subreddit
-    res.render('subtheme', { subtheme });
-})
+    res.render('subtheme.ejs', { subtheme }); // of {theme: subtheme}
+});
+
+app.get('/cats', (req, res) => {
+    const cats = ['Monty', 'Belly', 'Pepo'];
+    res.render('cats.ejs', { cats }); // or {allCats: cats}
+});
 
 app.listen(3000, (req, res) => {
     console.log('Starting to listen on port 3000');
-})
+});
 
 // express_ejs / views // run node index.js (works)
 // express_servers / express_ejs / views // run node express_ejs/index.js (will run index.js, but ejs will not work, as it will set path to views in the currecnt directory you are running command from): (express_servers/views) - will try to look for views in directory you are running node index.js from  
