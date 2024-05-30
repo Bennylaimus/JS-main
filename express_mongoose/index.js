aa01d86852a0388dabf131e968046a6c2268ca1e
Andrew Komorkovich <komorkovics@gmail.com>
2024-05-30 23:35:18 +0300

    connection mongooose

--- express_mongoose/index.js
+++ express_mongoose/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+    .then(() => {
+        console.log('MongoDB Connected');
+    })
+    .catch(err => {
+        console.log('Problem with MongoDB Connection!')
+        console.log(err)
+    });
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
